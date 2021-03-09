b4ba4d388deba0280cddc27a0e9592e00ef786d5
DJKirkman <57725196+DJKirkman@users.noreply.github.com>
2021-03-09 15:14:35 +0000

    Updated Raspberry Pi API
    -Added ability to adjust speed. Can pass a list of integers to indicate the speed for each motor or just an int to use that speed for all motors.
    -Added checking distance sensors using phidget sensors
    -Added paul_tidy to handle cleanup before termination of code

--- paul/paul_pi.py
+++ paul/paul_pi.py
@@ -1,6 +1,10 @@
 # API for Raspberry Pi Controller used to define Paul class
 # Python 2 (Requirement for Motor Controllers)
-
+from Phidget22.Phidget import *
+from Phidget22.Devices.DigitalInput import *
+from Phidget22.Devices.VoltageInput import *
+from Phidget22.Devices.DigitalOutput import *
+import time
 from motors import Motors
 
 class Paul(object):
@@ -16,27 +20,61 @@ class Paul(object):
         self.down_speed = 100
         
         # Threshold reading on top distance sensors before triggering
-        self.top_threshold = 10
+        self.top_threshold = 2.184
         # Threshold reading on bottom distance sensors before triggering
-        self.bottom_threshold = 10
+        self.bottom_threshold = 2.184
         
         # Note: The following should only be set if distance sensors are not attached,
         # they should be set to 0 if sensors are attached.
-        self.top_ds_reading = 20
-        self.bottom_ds_reading = 20
+        self.top_ds_reading = 0
+        self.bottom_ds_reading = 0
 
         # Create an instance of the Motors class used in SDP
         self.mc = Motors()
 
+        #Sensors
+        #define array of digital inputs to store readings from the bump sensors
+        self.bump_sensors = [DigitalInput() for i in range (0,6)]
+        #addressing the channels for the bump sensors
+        #0-2 are top sensors
+        #3-5 are bottom channels
+        for i in range(0,6):
+            self.bump_sensors[i].setChannel(i)
+            self.bump_sensors[i].openWaitForAttachment(5000)
+
+        #define array of analogue inputs to store readings from the ir sensors
+        self.ir_sensors = [VoltageInput() for i in range (0,6)]
+        #addressing the channels for the ir sensors
+        #0-2 are top sensors
+        #3-5 are bottom channels
+        for i in range(0,6):
+            self.ir_sensors[i].setChannel(i)
+            #this line will basically call the event handler when the readings change
+            self.ir_sensors[i].openWaitForAttachment(5000)
+
 
 # Movement Functions  
 
-    def start_motors(self,speed):
+    def start_motors(self,speed_input):
+        #If speed_input is an int then the speeds of all the motors should be the same
+        if type(speed_input) is int:
+            speeds = [speed_input, speed_input, speed_input, speed_input, speed_input, speed_input]
+            self.speed = speed_input
+        #If speed_input is a list then the speeds should be set different, set speed to a non int value to disable changing speed on that motor
+        elif type(speed_input) is list:
+            speeds = speed_input
+        #If list is not valid then set speeds to the current speed
+        else:
+            speeds = [self.speed, self.speed, self.speed, self.speed, self.speed, self.speed]
+        #ID's of all the motors to run
         motor_ids = [5,4,3,2,1,0]
-        self.speed = speed
         print 'Starting motors...'
-        for motor_id in motor_ids:
-            self.mc.move_motor(motor_id,self.speed)
+        #print speeds
+        for i in range(len(speeds)):
+        #for motor_id in motor_ids:
+            if type(speeds[i]) is int:
+                self.mc.move_motor(motor_ids[i],speeds[i])
+            #self.mc.move_motor(motor_id,self.speed)
 
     def stop_motors(self):
         print 'Stopping motors...'
@@ -59,25 +97,65 @@ class Paul(object):
         #where id is the port number on the encoder board that you wish to read
         return self.speed
 
+    #Returns true if the top distance sensors are within range of the top 
+    #Returns false otherwise
     def check_top_ds_reading(self):
+        #Set the top distance sensor reading to the average of the three IR sensors on the top
+        self.top_ds_reading = (self.ir_sensors[0].getVoltage() + self.ir_sensors[1].getVoltage() + self.ir_sensors[2].getVoltage())/3
+        #If the voltage of the ir sensors is above a threshold then return true
+        if(self.top_ds_reading >= self.top_threshold):
+            return True
+        #If any of the top bumper sensors have been activated then return true
+        if(self.bump_sensors[0].getState() == 1 or self.bump_sensors[1].getState() == 1 or self.bump_sensors[2].getState() == 1):
+            return True
+        #Else return false
         return False 
 
     def display_top_ds_reading(self):
-        print "Top DS Reading: --"
+        print "Top DS Reading: " + str(self.top_ds_reading)
     
 
     def get_top_ds_reading(self):
         return self.top_ds_reading
     
+    #Returns true if the top distance sensors are within range of the top 
+    #Returns false otherwise
     def check_bottom_ds_reading(self):
+        #Set the bottom distance sensor reading to the average of the three IR sensors on the bottom
+        self.bottom_ds_reading = (self.ir_sensors[3].getVoltage() + self.ir_sensors[4].getVoltage() + self.ir_sensors[5].getVoltage())/3
+        #If the voltage of the ir sensors is above a threshold then return true
+        if(self.bottom_ds_reading >= self.bottom_threshold):
+            return True
+        #If any of the bottom bumper sensors have been activated then return true
+        if(self.bump_sensors[3].getState() == 1 or self.bump_sensors[4].getState() == 1 or self.bump_sensors[5].getState() == 1):
+            return True
+        #Else return false
         return False 
 
     def display_bottom_ds_reading(self):
-        print "Bottom DS Reading: --" 
+        print "Bottom DS Reading: " + str(self.bottom_ds_reading)
 
     def get_bottom_ds_reading(self):
         return self.bottom_ds_reading
-    
+
+
+# Terminate script
+    def paul_tidy(self):
+        #Stop the motors
+        self.stop_motors()
+        
+        #Keyboard inerrupt may be needed to reset phidget
+        #try:
+        #    input("Press Enter to Stop\n")
+        #except (Exception, KeyboardInterrupt):
+        #    pass
+
+        #Close all the channels
+        for i in range (0, 6):
+            self.ir_sensors[i].close()
+            self.bump_sensorss[i].close()
+
+
 
 # Getters and Setters
 
